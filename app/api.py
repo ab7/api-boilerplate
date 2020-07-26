c3d12d6a94c52d1f906a7c6b476817f9e64003a9
Alex Baker <hello@alexbaker.io>
2020-07-25 20:15:29 -0600

    Implement static type checking

--- app/api.py
+++ app/api.py
@@ -1,7 +1,7 @@
-from typing import List
+from typing import List, Iterator
 
 from fastapi import Depends, FastAPI
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, sessionmaker  # type: ignore
 
 from app import models, schemas
 from app.database import SessionLocal
@@ -10,7 +10,7 @@ from app.database import SessionLocal
 app = FastAPI()
 
 
-def get_db():
+def get_db() -> Iterator[sessionmaker]:
     db = SessionLocal()
     try:
         yield db
@@ -19,18 +19,18 @@ def get_db():
 
 
 @app.get('/')
-async def root():
+async def root() -> dict:
     return {'message': 'Hello, World!'}
 
 
 @app.get('/users', response_model=List[schemas.User])
-async def users_get(offset: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def users_get(offset: int = 0, limit: int = 100, db: Session = Depends(get_db)) -> dict:
     users = db.query(models.User).offset(offset).limit(limit).all()
     return users
 
 
 @app.post('/users', response_model=schemas.User)
-def users_create(user: schemas.UserCreate, db: Session = Depends(get_db)):
+def users_create(user: schemas.UserCreate, db: Session = Depends(get_db)) -> dict:
     user = models.User(username=user.username, email=user.email)
     db.add(user)
     db.commit()
--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String  # type: ignore
 
 from app.database import Base
 
